342bd4068acf17f37a7e5e0a779d47e02346326d
wangenga <107638352+wangenga@users.noreply.github.com>
2026-01-29 20:44:00 +0300

    feat: integrate electron-reload for automatic reloading during development

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+try {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+    });
+}catch (_) {}
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 300, height: 400, frame: false, transparent: true,
